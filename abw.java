0753091d8161f4eb44df269eec36dd0dfe6bd9f8
Baradhwaj-A <baradh1452020@gmail.com>
2025-07-24 14:38:10 +0530

    suduko

--- abw.java
+++ abw.java
@@ -1,4 +1,37 @@
-public class SudokuSolver {
+public class Sudoku {
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        SudokuSolver solver = new SudokuSolver();
+        if (solver.solve(board)) {
+            System.out.println("Sudoku solved successfully!");
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int r = 0; r < 9; r++) {
+            for (int d = 0; d < 9; d++) {
+                System.out.print(board[r][d] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+class SudokuSolver {
     boolean solve(int[][] board) {
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
@@ -16,12 +49,12 @@ public class SudokuSolver {
         }
         return true;
     }
-    
+
     boolean isValid(int[][] board, int row, int col, int num) {
         // Check row, column & 3x3 box
         for (int i = 0; i < 9; i++) {
             if (board[row][i] == num || board[i][col] == num || 
-                board[3*(row/3)+i/3][3*(col/3)+i%3] == num) {
+                board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
                 return false;
             }
         }
